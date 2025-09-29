0bbef4536a7483f1098e78082efcd05cbcda4777
Tob-private <tobias.riiser.games@gmail.com>
2025-09-29 15:33:34 +0200

    changed base url

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "https://tob-private.github.io/The-Crater-of-Ciasom/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
